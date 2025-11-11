2e3c1612265d0c2759d23f8252512b02f1d4ed30
GlebGorlanov <gorlanovgleb@gmail.com>
2025-11-11 21:49:11 +0300

    Update Download.vue

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -3,40 +3,96 @@
         <div class="head rounded-[4vw] sm:rounded-[3.5vw] md:rounded-[3vw] lg:rounded-[2.3vw] xl:rounded-[2vw] 2xl:rounded-[1.5vw]">
             <span class="close text-[9vw] mr-[5vw] sm:text-[8.5vw] sm:mr-[4.5vw] md:text-[7vw] md:mr-[3.5vw] lg:text-[5vw] lg:mr-[2.5vw] xl:text-[3.2vw] xl:mr-[1.5vw] 2xl:text-[3vw] 2xl:mr-[1.3vw]" @click="closeDownload('closeDownload')">&#215;</span>
         </div>
+        
+        <div class="application flex items-center shrink-0" v-if="application === 'calculator'">
+            <img src="../assets/applications/Screenshots Calculator/Calculator0.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator1.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator2.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator3.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator4.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator5.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator6.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator7.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator8.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator9.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Calculator/Calculator10.png" alt="#" class="aspect-[1/1.464] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+        </div>
 
-        <div class="application flex items-center justify-center">
-            <iframe class="aspect-[1/1.464] frame w-[86%] rounded-[3vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]" src="https://cozy-douhua-274f99.netlify.app/"></iframe>
+        <div class="application cross_zero flex items-center shrink-0" v-else-if="application === 'cross_zero'">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero0.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero1.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero2.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero3.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero4.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero5.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero6.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero7.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero8.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
+            <img src="../assets/applications/Screenshots Cross_Zero/Cross_Zero9.png" alt="#" class="aspect-[1/1.1] w-[86%] rounded-[1.5vw] sm:rounded-[2.5vw] md:rounded-[2vw] lg:rounded-[1.5vw] xl:rounded-[1vw] 2xl:rounded-[1.5vw]">
         </div>
 
         <div class="windowsLoading flex items-center">
             <div class="windows flex items-center justify-center w-1/3 h-full rounded-bl-[4vw] sm:rounded-bl-[3.5vw] md:rounded-bl-[3vw] lg:rounded-bl-[2.3vw] xl:rounded-bl-[1.5vw] 2xl:rounded-bl-[1.3vw]">
-                <a href="https://github.com/GlebGorlanov/git_test/releases/download/v1.0.0/Calculator.Setup.1.0.0.exe" download class="flex items-center justify-center w-full h-full">
+                <a v-if="application === 'calculator'" href="https://github.com/GlebGorlanov/Applications/releases/download/Calculator_v1.0.0/Calculator.Setup.1.0.0-win64.exe" download class="flex items-center justify-center w-full h-full">
                     <span class="text-[4vw] sm:text-[3.8vw] md:text-[3vw] lg:text-[2vw] xl:text-[1.5vw] 2xl:text-[1.3vw]">Windows</span><img src="../assets/applications/zagruzka.png" class="w-[15%] ml-[10%] sm:w-[14.5%] sm:ml-[9%] md:w-[14%] md:ml-[6%] lg:w-[13%] lg:ml-[5%] xl:w-[12%] xl:ml-[5%] 2xl:w-[12%] 2xl:ml-[5%]">
                 </a>
+
+                <a v-else-if="application === 'cross_zero'" href="https://github.com/GlebGorlanov/Applications/releases/download/Tic_tac_toe_v1.0.0/Tic-Tac-Toe.Setup.1.0.0-win64.exe" download class="flex items-center justify-center w-full h-full">
+                    <span class="text-[4vw] sm:text-[3.8vw] md:text-[3vw] lg:text-[2vw] xl:text-[1.5vw] 2xl:text-[1.3vw]">Windows</span><img src="../assets/applications/zagruzka.png" class="w-[15%] ml-[10%] sm:w-[14.5%] sm:ml-[9%] md:w-[14%] md:ml-[6%] lg:w-[13%] lg:ml-[5%] xl:w-[12%] xl:ml-[5%] 2xl:w-[12%] 2xl:ml-[5%]">
+                </a>
+
+                <div class="filter backdrop-blur-2xl" v-show="os !== 'Windows'"></div>
             </div>
 
             <div class="linux flex items-center justify-center w-1/3 h-full">
-                <a href="#" class="flex items-center justify-center w-full h-full">
+                <a v-if="application === 'calculator'" href="#" class="flex items-center justify-center w-full h-full">
                     <span class="text-[4vw] sm:text-[3.8vw] md:text-[3vw] lg:text-[2vw] xl:text-[1.5vw] 2xl:text-[1.3vw]">Linux</span><img src="../assets/applications/zagruzka.png" class="w-[15%] ml-[10%] sm:w-[14.5%] sm:ml-[9%] md:w-[14%] md:ml-[6%] lg:w-[13%] lg:ml-[5%] xl:w-[12%] xl:ml-[5%] 2xl:w-[12%] 2xl:ml-[5%]">
                 </a>
+
+                <a v-else-if="application === 'cross_zero'" href="#" class="flex items-center justify-center w-full h-full">
+                    <span class="text-[4vw] sm:text-[3.8vw] md:text-[3vw] lg:text-[2vw] xl:text-[1.5vw] 2xl:text-[1.3vw]">Linux</span><img src="../assets/applications/zagruzka.png" class="w-[15%] ml-[10%] sm:w-[14.5%] sm:ml-[9%] md:w-[14%] md:ml-[6%] lg:w-[13%] lg:ml-[5%] xl:w-[12%] xl:ml-[5%] 2xl:w-[12%] 2xl:ml-[5%]">
+                </a>
+
+                <div class="filter backdrop-blur-2xl" v-show="os !== 'Linux'"></div>
             </div>
 
             <div class="macos flex items-center justify-center w-1/3 h-full rounded-br-[4vw] sm:rounded-br-[3.5vw] md:rounded-br-[3vw] lg:rounded-br-[2.3vw] xl:rounded-br-[1.5vw] 2xl:rounded-br-[1.3vw]">
-                <a href="#" class="flex items-center justify-center w-full h-full">
+                <a v-if="application === 'calculator'" href="#" class="flex items-center justify-center w-full h-full">
                     <span class="text-[4vw] sm:text-[3.8vw] md:text-[3vw] lg:text-[2vw] xl:text-[1.5vw] 2xl:text-[1.3vw]">MacOS</span><img src="../assets/applications/zagruzka.png" class="w-[15%] ml-[10%] sm:w-[14.5%] sm:ml-[9%] md:w-[14%] md:ml-[6%] lg:w-[13%] lg:ml-[5%] xl:w-[12%] xl:ml-[5%] 2xl:w-[12%] 2xl:ml-[5%]">
                 </a>
+
+                <a v-else-if="application === 'cross_zero'" href="#" class="flex items-center justify-center w-full h-full">
+                    <span class="text-[4vw] sm:text-[3.8vw] md:text-[3vw] lg:text-[2vw] xl:text-[1.5vw] 2xl:text-[1.3vw]">MacOS</span><img src="../assets/applications/zagruzka.png" class="w-[15%] ml-[10%] sm:w-[14.5%] sm:ml-[9%] md:w-[14%] md:ml-[6%] lg:w-[13%] lg:ml-[5%] xl:w-[12%] xl:ml-[5%] 2xl:w-[12%] 2xl:ml-[5%]">
+                </a>
+
+                <div class="filter backdrop-blur-2xl" v-show="os !== 'MacOs'"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-    import { defineEmits } from 'vue';
+    import { ref, defineProps, defineEmits, onMounted } from 'vue';
+
+    const props = defineProps(['application']);
     const emit = defineEmits(['closeDownload']);
+    const os = ref('');
 
     const closeDownload = (txt) => {
         emit('closeDownload', txt);
     };
+
+    onMounted(() => {
+        const platform = navigator.userAgent;
+
+        if(platform.includes('Win')) {
+            os.value = 'Windows';
+        } else if(platform.includes('Linux')) {
+            os.value = 'Linux';
+        } else if(platform.includes('Mac')) {
+            os.value = 'MacOs';
+        };
+    });
 </script>
 
 <style scoped>
@@ -79,13 +135,64 @@
     }
 
     .application {
+        display: flex;
+        flex-direction: column;
+
         width: 100%;
         height: 100%;
+
+        overflow-y: auto;
+    }
+
+    .application::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    .application::-webkit-scrollbar-track {
+        border-radius: 0px;
+    }
+
+    .application::-webkit-scrollbar-thumb {
+        background-color: rgba(192, 192, 192, 0.6);
+        border-radius: 0px;
+    }
+
+    .application::-webkit-scrollbar-thumb:hover {
+        background-color: rgba(245, 245, 245, 0.7);
+    }
+
+    .application > img {
+        margin: 6px 0;
+    }
+
+    .cross_zero > img {
+        margin: 10px 0;
     }
 
     .windowsLoading {
         width: 100%;
         height: 10%;
+    }
+
+    .windows, .linux, .macos {
+        position: relative;
+        overflow: hidden;
+        z-index: 0;
+    }
+
+    .filter {
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        width: 100%;
+        height: 100%;
+
+        background-color: rgba(255, 255, 255, 0.2);
+
+        z-index: 10;
+        pointer-events: auto;
+        cursor: not-allowed;
     }
 
     .windowsLoading > div {
@@ -113,5 +220,3 @@
         border-right: 1px solid rgba(255, 255, 255, 0.5);
     }
 </style>
-
-
